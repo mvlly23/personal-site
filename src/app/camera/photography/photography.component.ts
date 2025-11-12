5d7d5492c4800e75b8bbbeb866a6b0e8d1a8347d
mvlly23 <mollybraswell232@gmail.com>
2025-11-12 11:42:01 -0500

    Updated information
    Updated resume
    
    Added new pics
    
    Changed videography to a dedicated page for new left dropouts

--- src/app/camera/photography/photography.component.ts
+++ src/app/camera/photography/photography.component.ts
@@ -13,6 +13,7 @@ import { Component, Input } from "@angular/core";
 
 export class PhotographyComponent {
   aquarium = ['/P1011568.jpg', '/P1011571.jpg', '/P1011584.jpg', "/P1011585.jpg", "/P1011612.jpg", "/P1011620.jpg", "/P1011628.jpg", "/P1011631.jpg", "/P1011639.jpg"];
+  asheville = ['/P1013024.jpg', '/P1013035.jpg', '/P1013044.jpg', '/P1013030.jpg', '/P1013051.jpg'];
   zoo = ["/P1011468.JPG", "/P1011472.JPG", "/P1011512.jpg", "/P1011532.jpg", "/P1011538.jpg"];
   neature = ["/P1000162.jpg", "/P1000189.jpg", "/P1000219.jpg", "/P1000241.jpg", "/P1000249.jpg", "/P1000253.jpg"];
   renaissance_park = ["/P1012046.jpg", "P1012075.jpg", "/P1012036.jpg", "/P1012070.jpg", "/P1012111.jpg", "/P1012012.jpg", "/P1012130.jpg"];
